6401ae0e71d1601fab416aeac54d06d1095e5d07
Your Name <you@example.com>
2022-06-29 16:52:57 +0700

    ftm

---
+++ demo.js
@@ -0,0 +1,10 @@
+function foottometer(foot){
+    let meter=0.305*foot;
+    return meter;
+}
+function metertofoot(meter){
+    let foot=3.279*meter;
+    return foot;
+}
+console.log(foottometer(1))
+console.log(metertofoot(20))
